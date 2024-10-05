ed7d8db2d52414d31869e67c6e2328c1ea6d4429
Mamadou Abdoulaye DIOP <diop20@hotmail.com>
2024-10-05 06:38:02 +0000

    Remove generated files

--- CreateBucketTask.cs
+++ CreateBucketTask.cs
@@ -17,13 +17,14 @@ namespace S3Operations
 
             try
             {
-				IAmazonS3 s3Client = null;
-				
-				// TODO 1: Create an Amazon S3 client to interact with the service
-				
+                IAmazonS3 s3Client = null;
 
-				// End TODO1
-				
+                // TODO 1: Create an Amazon S3 client to interact with the service
+
+                s3Client = new AmazonS3Client(region: Amazon.RegionEndpoint.APSoutheast1);
+
+                // End TODO1
+
                 Console.WriteLine("Verifying that the bucket name is valid...");
                 // Verify that the bucket exists. The code will exit
                 // if the name is not valid for a new bucket.
@@ -34,7 +35,7 @@ namespace S3Operations
 
                 // Pause until the the bucket is in the account
                 Console.WriteLine("\nConfirm that the bucket exists...");
-                await VerifyBucket (s3Client, configSettings.BucketName);
+                await VerifyBucket(s3Client, configSettings.BucketName);
             }
             catch (Exception e)
             {
@@ -46,27 +47,27 @@ namespace S3Operations
 
         async Task VerifyBucketName(IAmazonS3 s3Client, string bucketName)
         {
-			bool exists = false;
+            bool exists = false;
 
-			// Start TODO 2: enter a command that will check if a bucket already exists in AWS
-			// with the name built from your appsettings.json file input.
+            // Start TODO 2: enter a command that will check if a bucket already exists in AWS
+            // with the name built from your appsettings.json file input.
+            exists = await AmazonS3Util.DoesS3BucketExistV2Async(s3Client, bucketName);
 
+            // End TODO2
 
-			// End TODO2
-
-			if (exists)
-			{
-				// DoesS3BucketExistV2Async returns true if the bucket exists, but that does not
-				// necessarily mean it belongs to your account as the method catches AccessDenied
-				// and other exceptions. 
-				// See https://github.com/aws/aws-sdk-net/blob/bd7ccb4f12feab0499d454371b6623c7798e371f/sdk/src/Services/S3/Custom/Util/AmazonS3Util.cs#L604
-				Console.WriteLine("This bucket already exists in your, or someone else's, account. Exiting because there is nothing further to do!");
-				Environment.Exit(0);
-			}
-			else
-			{
-				Console.WriteLine("The bucket does not exist.");
-			}
+            if (exists)
+            {
+                // DoesS3BucketExistV2Async returns true if the bucket exists, but that does not
+                // necessarily mean it belongs to your account as the method catches AccessDenied
+                // and other exceptions. 
+                // See https://github.com/aws/aws-sdk-net/blob/bd7ccb4f12feab0499d454371b6623c7798e371f/sdk/src/Services/S3/Custom/Util/AmazonS3Util.cs#L604
+                Console.WriteLine("This bucket already exists in your, or someone else's, account. Exiting because there is nothing further to do!");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("The bucket does not exist.");
+            }
         }
 
         async Task CreateBucket(IAmazonS3 s3Client, string bucketName)
@@ -77,7 +78,8 @@ namespace S3Operations
 
             // Start TODO 3: Create a new bucket in the users current region
 
-
+            var putBucketRequest = new PutBucketRequest { BucketName = bucketName };
+            await s3Client.PutBucketAsync(putBucketRequest);
             // End TODO 3
 
             Console.WriteLine("Success!");
@@ -88,7 +90,10 @@ namespace S3Operations
             // Start TODO 4: Complete the function so that it will
             // pause and only proceed after the bucket exists
 
-
+            while (! await AmazonS3Util.DoesS3BucketExistV2Async(s3Client, bucketName))
+            {
+               await Task.Delay(5);
+            }
             // End TODO 4
             Console.WriteLine($"The bucket: {bucketName} is now available.");
         }
